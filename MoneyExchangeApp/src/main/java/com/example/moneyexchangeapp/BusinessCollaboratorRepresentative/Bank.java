2988cc64dadf437624b2205e94b90f9af351980b
Zayed Bin Zaman <2420960@iub.edu.bd>
2025-12-09 23:58:34 +0600

    Update Bank.java

--- MoneyExchangeApp/src/main/java/com/example/moneyexchangeapp/BusinessCollaboratorRepresentative/Bank.java
+++ MoneyExchangeApp/src/main/java/com/example/moneyexchangeapp/BusinessCollaboratorRepresentative/Bank.java
@@ -20,7 +20,6 @@ public class Bank {
         return false;
     }
 
-    // Getters and Setters
     public String getBankName() { return bankName; }
     public void setBankName(String bankName) { this.bankName = bankName; }
 
